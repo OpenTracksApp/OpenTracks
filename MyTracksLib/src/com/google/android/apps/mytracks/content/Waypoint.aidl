cd5cb9093ca6c3996dfb93d36930ab2fd070f390
Rodrigo Damazio <Rodrigo Damazio@localhost>
2011-03-23 22:07:21 -0700

    Adding first version of My Tracks i18n tool.
    Since this is a first proposal, I'm not yet modifying the xml files to pass validation, except for a small example.
    Also keep in mind that the annotations in the XML (rev in a string tag, or KEEP_PARENT) would be inserted automatically by the "translate" module.

---
+++ MyTracksLib/src/com/google/android/apps/mytracks/content/Waypoint.aidl
@@ -0,0 +1,2 @@
+package com.google.android.apps.mytracks.content;
+parcelable Waypoint;
---
+++ MyTracksTest/src/com/google/android/apps/mytracks/services/DefaultTrackNameFactoryTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.android.apps.mytracks.services;
+
+import android.content.Context;
+import android.test.AndroidTestCase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Tests {@link DefaultTrackNameFactory}
+ * 
+ * @author Matthew Simmons
+ */
+public class DefaultTrackNameFactoryTest extends AndroidTestCase {
+  /**
+   * A version of the factory which allows us to supply our own answer as to
+   * whether a timestamp-based track name should be used.
+   */
+  private static class MockDefaultTrackNameFactory
+      extends DefaultTrackNameFactory {
+    private final boolean useTimestamp;
+
+    MockDefaultTrackNameFactory(Context context, boolean useTimestamp) {
+      super(context);
+      this.useTimestamp = useTimestamp;
+    }
+    
+    @Override
+    protected boolean useTimestampTrackName() {
+      return useTimestamp;
+    }
+  }
+  
+  private static final long TIMESTAMP = 1288213406000L;
+
+  public void testTimestampTrackName() {
+    DefaultTrackNameFactory factory =
+        new MockDefaultTrackNameFactory(getContext(), true);
+    
+    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    assertEquals(formatter.format(new Date(TIMESTAMP)),
+        factory.newTrackName(1, TIMESTAMP));
+  }
+
+  public void testIncrementingTrackName() {
+    DefaultTrackNameFactory factory =
+        new MockDefaultTrackNameFactory(getContext(), false);
+
+    assertEquals("Track 1", factory.newTrackName(1, TIMESTAMP));
+  }
+}
