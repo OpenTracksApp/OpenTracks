acf5c0931f95b67693b11b4ac335d265b31114d0
Dominik R?ttsches <d.roettsches@gmail.com>
2011-08-25 15:12:33 +0300

    Addressing review comments for StrideReadings class and related test cases: - removing inner class, and recording of heartbeat timestamps - fixing issue with rollover by replacing % operator with modulo operation - comparing problematic firmware id against byte[] instead of String - removing zephyr packet debug loggin - not reassigning protobuffers Builders - moving decision to use workaround into setCadence function - license and javadoc author for testcase class - proposal: using introspection to access private fields in testcase in order to determine expected availablity of values from getCadence, alternatively modify access to protected. - modifying rollover test, hopefully more clear now. - adding asserts during the loop in this test case.

--- MyTracks/src/com/google/android/apps/mytracks/services/sensors/StrideReadings.java
+++ MyTracks/src/com/google/android/apps/mytracks/services/sensors/StrideReadings.java
@@ -19,43 +19,33 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Storage of a history of readings of the Zephyr stride counter,
- * in order to derive a correct cadence value from it,
- * working around an issue with the HxM's firmware. 
- *
+ * A history of Zephyr stride counter reading.
+ * These can be used as an alternate method to calculate the correct cadence.
+ * This is a work around for an issue with some HxM firmware.
  * @author Dominik Ršttsches
  */
 public class StrideReadings {
-  private static class StrideReading {
-    // TODO: Check whether 1Hz assumption is okay for cadence calculation
-    // otherwise use timeMs, which is taken from heart beat timestamp.
-    @SuppressWarnings("unused")
-    public int timeMs;
-    public int numStrides;
-
-    StrideReading(int newTimeMs, int newNumStrides) {
-      timeMs = newTimeMs;
-      numStrides = newNumStrides;
-    }
-  }
 
   private static final int NUM_READINGS_FOR_AVERAGE = 10;
   private static final int MIN_READINGS_FOR_AVERAGE = 5;
 
   protected static final int CADENCE_NOT_AVAILABLE = -1;
 
-  private List<StrideReading> strideReadingsHistory;
+  // TODO: Check whether 1Hz assumption is okay for cadence calculation
+  // otherwise add heart beat timestamp to this list and compute
+  // cadence from these timestamps.
+  private List<Integer> strideReadingsHistory;
 
   public StrideReadings() {
-    strideReadingsHistory = new LinkedList<StrideReading>();
+    strideReadingsHistory = new LinkedList<Integer>();
   }
 
-  public void updateStrideReading(int timeInMs, int numStrides) {
+  public void updateStrideReading(int numStrides) {
     // HRM/HxM documentation says, transmission frequency is 1 Hz, 
     // let's keep last NUM_READINGS_FOR_AVERAGE readings.
     // TODO: Calibrate this using a reliable footpod / cadence sensor, 
     // otherwise use heartbeat timestamp for calculation. 
-    strideReadingsHistory.add(0, new StrideReading(timeInMs, numStrides));
+    strideReadingsHistory.add(0, numStrides);
     while(strideReadingsHistory.size() > NUM_READINGS_FOR_AVERAGE) {
       strideReadingsHistory.remove(strideReadingsHistory.size()-1);
     }
@@ -66,14 +56,18 @@ public class StrideReadings {
       // Bail out if we cannot really get a meaningful average yet.
       return CADENCE_NOT_AVAILABLE;
     }
-    // compute assuming 1 stride reading/second
+    // Compute assuming 1 stride reading/second.
     int timeSinceOldestReadingSecs = strideReadingsHistory.size() - 1; 
-    int stridesThen = strideReadingsHistory
-      .get(strideReadingsHistory.size()-1).numStrides;
-    int stridesNow = strideReadingsHistory
-      .get(0).numStrides;
-    // Contrary to documentation stride value seems to roll over every 127 strides.
-    return Math.round( (float)((stridesNow - stridesThen) % 127) / 
+    int stridesThen = strideReadingsHistory.get(strideReadingsHistory.size()-1);
+    int stridesNow = strideReadingsHistory.get(0);
+    // Contrary to documentation stride value seems to roll over at 128.
+    return Math.round( (float)(mod((stridesNow - stridesThen), 128)) /
         timeSinceOldestReadingSecs * 60);
+  }
+
+  private int mod(int x, int y)
+  {
+      int result = x % y;
+      return result < 0? result + y : result;
   }
 }
