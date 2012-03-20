0bcdab1958475453be062294fc153fc5122223b6
Jimmy Shih <jshih@google.com>
2012-03-20 10:45:03 -0700

    merge heads

--- MyTracks/src/com/google/android/apps/mytracks/TrackList.java
+++ MyTracks/src/com/google/android/apps/mytracks/TrackList.java
@@ -17,6 +17,7 @@ package com.google.android.apps.mytracks;
 
 import static com.google.android.apps.mytracks.Constants.TAG;
 
+import com.google.android.apps.mytracks.content.MyTracksProviderUtils;
 import com.google.android.apps.mytracks.content.TracksColumns;
 import com.google.android.apps.mytracks.io.file.SaveActivity;
 import com.google.android.apps.mytracks.io.file.TrackWriterFactory.TrackFileFormat;
@@ -24,6 +25,8 @@ import com.google.android.apps.mytracks.io.sendtogoogle.SendRequest;
 import com.google.android.apps.mytracks.io.sendtogoogle.UploadServiceChooserActivity;
 import com.google.android.apps.mytracks.services.ServiceUtils;
 import com.google.android.apps.mytracks.services.TrackRecordingServiceConnection;
+import com.google.android.apps.mytracks.util.ApiAdapterFactory;
+import com.google.android.apps.mytracks.util.DialogUtils;
 import com.google.android.apps.mytracks.util.PlayTrackUtils;
 import com.google.android.apps.mytracks.util.StringUtils;
 import com.google.android.maps.mytracks.R;
@@ -31,15 +34,13 @@ import com.google.android.maps.mytracks.R;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ListActivity;
-import android.content.ContentUris;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.Parcelable;
 import android.util.Log;
 import android.view.ContextMenu;
@@ -68,6 +69,8 @@ public class TrackList extends ListActivity
 
   private static final int DIALOG_INSTALL_EARTH_ID = 0;
   private static final int DIALOG_EXPORT_ALL_ID = 1;
+  private static final int DIALOG_DELETE_ALL_ID = 2;
+  private static final int DIALOG_DELETE_CURRENT_ID = 3;
   
   private int contextPosition = -1;
   private long trackId = -1;
@@ -226,10 +229,7 @@ public class TrackList extends ListActivity
         startSaveActivity(TrackFileFormat.TCX, false);
         return true;        
       case Constants.MENU_DELETE:
-        Uri uri = ContentUris.withAppendedId(TracksColumns.CONTENT_URI, trackId);
-        intent = new Intent(Intent.ACTION_DELETE)
-            .setDataAndType(uri, TracksColumns.CONTENT_ITEMTYPE);
-        startActivity(intent);
+        showDialog(DIALOG_DELETE_CURRENT_ID);
         return true;
       default:
         Log.w(TAG, "Unknown menu item: " + item.getItemId() + "(" + item.getTitle() + ")");
@@ -255,8 +255,7 @@ public class TrackList extends ListActivity
   public void onClick(View v) {
     switch (v.getId()) {
       case R.id.tracklist_btn_delete_all: {
-        Handler h = new DeleteAllTracks(this, null);
-        h.handleMessage(null);
+        showDialog(DIALOG_DELETE_ALL_ID);
         break;
       }
       case R.id.tracklist_btn_export_all: {
@@ -354,6 +353,36 @@ public class TrackList extends ListActivity
             .setSingleChoiceItems(choices, 0, null)
             .setTitle(R.string.track_list_export_all)
             .create();
+      case DIALOG_DELETE_ALL_ID:
+        return DialogUtils.createConfirmationDialog(this,
+            R.string.track_list_delete_all_confirm_message, new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialog, int which) {
+                MyTracksProviderUtils.Factory.get(TrackList.this).deleteAllTracks();
+                SharedPreferences sharedPreferences = getSharedPreferences(
+                    Constants.SETTINGS_NAME, Context.MODE_PRIVATE);
+                Editor editor = sharedPreferences.edit();
+                // TODO: Go through data manager
+                editor.putLong(getString(R.string.selected_track_key), -1L);
+                ApiAdapterFactory.getApiAdapter().applyPreferenceChanges(editor);
+              }
+            });
+      case DIALOG_DELETE_CURRENT_ID:
+        return DialogUtils.createConfirmationDialog(this,
+            R.string.track_list_delete_track_confirm_message, new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialog, int which) {
+                MyTracksProviderUtils.Factory.get(TrackList.this).deleteTrack(trackId);
+                // If the deleted track was selected, unselect it.
+                String selectedTrackKey = getString(R.string.selected_track_key);
+                SharedPreferences sharedPreferences = getSharedPreferences(
+                    Constants.SETTINGS_NAME, Context.MODE_PRIVATE);
+                if (sharedPreferences.getLong(selectedTrackKey, -1L) == trackId) {
+                  Editor editor = sharedPreferences.edit().putLong(selectedTrackKey, -1L);
+                  ApiAdapterFactory.getApiAdapter().applyPreferenceChanges(editor);
+                }
+              }
+            });
       default:
         return null;
     }
