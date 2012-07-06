9a24f12e11342636b17685b454bf51ddf78a4892
youtaol <youtaol@localhost>
2012-07-06 14:41:15 +0800

    Add test case to check the mark split logic.

---
+++ MyTracks/src/com/google/android/apps/mytracks/AbstractMyTracksActivity.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2012 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.android.apps.mytracks;
+
+import com.google.android.apps.mytracks.util.ApiAdapterFactory;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v4.app.FragmentActivity;
+import android.view.MenuItem;
+
+/**
+ * An abstract class for all My Tracks activities.
+ * 
+ * @author Jimmy Shih
+ */
+public class AbstractMyTracksActivity extends FragmentActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setVolumeControlStream(TextToSpeech.Engine.DEFAULT_STREAM);
+    ApiAdapterFactory.getApiAdapter().configureActionBarHomeAsUp(this);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    if (item.getItemId() != android.R.id.home) {
+      return super.onOptionsItemSelected(item);
+    }
+    onHomeSelected();
+    return true;
+  }
+
+  /**
+   * Callback when the home menu item is selected.
+   */
+  protected void onHomeSelected() {
+    finish();
+  }
+}
