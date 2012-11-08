3f9fd82b92ba2c0aca488a1436168381e816e369
youtaol <youtaol@localhost>
2012-11-08 17:02:28 +0800

    Add stress test and move sendor test to package com.google.android.apps.mytracks.endtoendtest.others.

---
+++ MyTracks/src/com/google/android/apps/mytracks/content/DataSource.java
@@ -0,0 +1,209 @@
+/*
+ * Copyright 2011 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.android.apps.mytracks.content;
+
+import static com.google.android.apps.mytracks.Constants.MAX_LOCATION_AGE_MS;
+
+import com.google.android.apps.mytracks.Constants;
+import com.google.android.apps.mytracks.services.MyTracksLocationManager;
+import com.google.android.apps.mytracks.util.GoogleLocationUtils;
+import com.google.android.maps.mytracks.R;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.database.ContentObserver;
+import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Data source on the phone.
+ * 
+ * @author Rodrigo Damazio
+ */
+public class DataSource {
+
+  private static final int NETWORK_PROVIDER_MIN_TIME = 5 * 60 * 1000; // 5 minutes
+  private static final String TAG = DataSource.class.getSimpleName();
+
+  private final Context context;
+  private final ContentResolver contentResolver;
+  private final MyTracksLocationManager myTracksLocationManager;
+  private final SensorManager sensorManager;
+  private final SharedPreferences sharedPreferences;
+  
+  public DataSource(Context context) {
+    this.context = context;
+    contentResolver = context.getContentResolver();
+    myTracksLocationManager = new MyTracksLocationManager(context);
+    sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+    sharedPreferences = context.getSharedPreferences(Constants.SETTINGS_NAME, Context.MODE_PRIVATE);
+  }
+
+  public void close() {
+    myTracksLocationManager.close();
+  }
+
+  public boolean isAllowed() {
+    return myTracksLocationManager.isAllowed();
+  }
+
+  /**
+   * Registers a content observer.
+   * 
+   * @param uri the uri
+   * @param observer the observer
+   */
+  public void registerContentObserver(Uri uri, ContentObserver observer) {
+    contentResolver.registerContentObserver(uri, false, observer);
+  }
+
+  /**
+   * Unregisters a content observer.
+   * 
+   * @param observer the observer
+   */
+  public void unregisterContentObserver(ContentObserver observer) {
+    contentResolver.unregisterContentObserver(observer);
+  }
+
+  /**
+   * Registers a location listener.
+   * 
+   * @param listener the listener
+   */
+  public void registerLocationListener(LocationListener listener) {
+    // Check if the GPS provider exists
+    if (myTracksLocationManager.getProvider(LocationManager.GPS_PROVIDER) == null) {
+      listener.onProviderDisabled(LocationManager.GPS_PROVIDER);
+      unregisterLocationListener(listener);
+      return;
+    }
+
+    // Listen for GPS location
+    myTracksLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
+
+    // Update the listener with the current provider state
+    if (myTracksLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+      listener.onProviderEnabled(LocationManager.GPS_PROVIDER);
+    } else {
+      listener.onProviderDisabled(LocationManager.GPS_PROVIDER);
+    }
+
+    // Listen for network location
+    try {
+      myTracksLocationManager.requestLocationUpdates(
+          LocationManager.NETWORK_PROVIDER, NETWORK_PROVIDER_MIN_TIME, 0, listener);
+    } catch (RuntimeException e) {
+      // Network location is optional, so just log the exception
+      Log.w(TAG, "Could not register for network location.", e);
+    }
+  }
+
+  /**
+   * Unregisters a location listener.
+   * 
+   * @param listener the listener
+   */
+  public void unregisterLocationListener(LocationListener listener) {
+    myTracksLocationManager.removeUpdates(listener);
+  }
+
+  /**
+   * Gets the last known location.
+   */
+  public Location getLastKnownLocation() {
+    Location location = myTracksLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    if (!isLocationRecent(location)) {
+      // Try network location
+      location = myTracksLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+      String toast;
+      if (isLocationRecent(location)) {
+        toast = context.getString(R.string.my_location_approximate_location);
+      } else {
+        String setting = context.getString(
+            GoogleLocationUtils.isAvailable(context) ? R.string.gps_google_location_settings
+                : R.string.gps_location_access);
+        toast = context.getString(R.string.my_location_no_gps, setting);
+      }
+      Toast.makeText(context, toast, Toast.LENGTH_LONG).show();
+    }
+    return location;
+  }
+
+  /**
+   * Returns true if the location is recent.
+   * 
+   * @param location the location
+   */
+  private boolean isLocationRecent(Location location) {
+    if (location == null) {
+      return false;
+    }
+    return location.getTime() > System.currentTimeMillis() - MAX_LOCATION_AGE_MS;
+  }
+
+  /**
+   * Registers a heading listener.
+   * 
+   * @param listener the listener
+   */
+  public void registerHeadingListener(SensorEventListener listener) {
+    Sensor heading = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+    if (heading == null) {
+      Log.d(TAG, "No heading sensor.");
+      return;
+    }
+    sensorManager.registerListener(listener, heading, SensorManager.SENSOR_DELAY_UI);
+  }
+
+  /**
+   * Unregisters a heading listener.
+   * 
+   * @param listener the listener
+   */
+  public void unregisterHeadingListener(SensorEventListener listener) {
+    sensorManager.unregisterListener(listener);
+  }
+
+  /**
+   * Registers a shared preference change listener.
+   * 
+   * @param listener the listener
+   */
+  public void registerOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener) {
+    sharedPreferences.registerOnSharedPreferenceChangeListener(listener);
+  }
+
+  /**
+   * Unregisters a shared preference change listener.
+   * 
+   * @param listener the listener
+   */
+  public void unregisterOnSharedPreferenceChangeListener(
+      OnSharedPreferenceChangeListener listener) {
+    sharedPreferences.unregisterOnSharedPreferenceChangeListener(listener);
+  }
+}
---
+++ MyTracks/src/com/google/android/apps/mytracks/io/file/SaveActivity.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.android.apps.mytracks.io.file;
+
+import com.google.android.apps.mytracks.io.file.TrackWriterFactory.TrackFileFormat;
+import com.google.android.apps.mytracks.util.DialogUtils;
+import com.google.android.apps.mytracks.util.IntentUtils;
+import com.google.android.maps.mytracks.R;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import java.io.File;
+
+/**
+ * An activity for saving tracks to the SD card. If saving a specific track,
+ * option to save it to a temp directory and play the track afterward.
+ * 
+ * @author Rodrigo Damazio
+ */
+public class SaveActivity extends Activity {
+
+  public static final String EXTRA_TRACK_FILE_FORMAT = "track_file_format";
+  public static final String EXTRA_TRACK_ID = "track_id";
+  public static final String EXTRA_PLAY_TRACK = "play_track";
+
+  public static final String GOOGLE_EARTH_KML_MIME_TYPE = "application/vnd.google-earth.kml+xml";
+  public static final String GOOGLE_EARTH_PACKAGE = "com.google.earth";
+  public static final String GOOGLE_EARTH_MARKET_URL = "market://details?id="
+      + GOOGLE_EARTH_PACKAGE;
+  private static final String
+      GOOGLE_EARTH_TOUR_FEATURE_ID = "com.google.earth.EXTRA.tour_feature_id";
+  private static final String GOOGLE_EARTH_CLASS = "com.google.earth.EarthActivity";
+
+  private static final int DIALOG_PROGRESS_ID = 0;
+  private static final int DIALOG_RESULT_ID = 1;
+
+  private TrackFileFormat trackFileFormat;
+  private long trackId;
+  private boolean playTrack;
+
+  private SaveAsyncTask saveAsyncTask;
+  private ProgressDialog progressDialog;
+
+  // result from the AsyncTask
+  private boolean success;
+  
+  // message id from the AsyncTask
+  private int messageId;
+  
+  // saved file path from the AsyncTask
+  private String savedPath;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    Intent intent = getIntent();
+    trackFileFormat = intent.getParcelableExtra(EXTRA_TRACK_FILE_FORMAT);
+    trackId = intent.getLongExtra(EXTRA_TRACK_ID, -1L);
+    playTrack = intent.getBooleanExtra(EXTRA_PLAY_TRACK, false);
+
+    Object retained = getLastNonConfigurationInstance();
+    if (retained instanceof SaveAsyncTask) {
+      saveAsyncTask = (SaveAsyncTask) retained;
+      saveAsyncTask.setActivity(this);
+    } else {
+      saveAsyncTask = new SaveAsyncTask(this, trackFileFormat, trackId, playTrack);
+      saveAsyncTask.execute();
+    }
+  }
+
+  @Override
+  public Object onRetainNonConfigurationInstance() {
+    saveAsyncTask.setActivity(null);
+    return saveAsyncTask;
+  }
+
+  @Override
+  protected Dialog onCreateDialog(int id) {
+    switch (id) {
+      case DIALOG_PROGRESS_ID:
+        progressDialog = DialogUtils.createHorizontalProgressDialog(this,
+            R.string.external_storage_save_progress_message,
+            new DialogInterface.OnCancelListener() {
+                @Override
+              public void onCancel(DialogInterface dialog) {
+                saveAsyncTask.cancel(true);
+                finish();
+              }
+            });
+        return progressDialog;
+      case DIALOG_RESULT_ID:
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+            .setCancelable(true)
+            .setIcon(success 
+                ? android.R.drawable.ic_dialog_info : android.R.drawable.ic_dialog_alert)
+            .setMessage(messageId)
+            .setOnCancelListener(new DialogInterface.OnCancelListener() {
+              @Override
+              public void onCancel(DialogInterface dialog) {
+                dialog.dismiss();
+                onPostResultDialog();
+              }
+            })
+            .setPositiveButton(R.string.generic_ok, new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialog, int arg1) {
+                dialog.dismiss();
+                onPostResultDialog();
+              }
+            })
+            .setTitle(success ? R.string.generic_success_title : R.string.generic_error_title);
+
+        if (success && trackId != -1L && !playTrack && savedPath != null) {
+          builder.setNegativeButton(
+              R.string.share_track_share_file, new DialogInterface.OnClickListener() {
+                  @Override
+                public void onClick(DialogInterface dialog, int which) {
+                  Intent intent = IntentUtils.newShareFileIntent(
+                      SaveActivity.this, trackId, savedPath, trackFileFormat);
+                  startActivity(
+                      Intent.createChooser(intent, getString(R.string.share_track_picker_title)));
+                  finish();
+                }
+              });
+        }
+        return builder.create();
+      default:
+        return null;
+    }
+  }
+
+  /**
+   * Invokes when the associated AsyncTask completes.
+   *
+   * @param isSuccess true if the AsyncTask is successful
+   * @param aMessageId the id of the AsyncTask message
+   * @param aSavedPath the path of the saved file
+   */
+  public void onAsyncTaskCompleted(boolean isSuccess, int aMessageId, String aSavedPath) {
+    this.success = isSuccess;
+    this.messageId = aMessageId;
+    this.savedPath = aSavedPath;
+    removeDialog(DIALOG_PROGRESS_ID);
+    showDialog(DIALOG_RESULT_ID);
+  }
+
+  /**
+   * Shows the progress dialog.
+   */
+  public void showProgressDialog() {
+    showDialog(DIALOG_PROGRESS_ID);
+  }
+
+  /**
+   * Sets the progress dialog value.
+   *
+   * @param number the number of points saved
+   * @param max the maximum number of points
+   */
+  public void setProgressDialogValue(int number, int max) {
+    if (progressDialog != null) {
+      progressDialog.setIndeterminate(false);
+      progressDialog.setMax(max);
+      progressDialog.setProgress(Math.min(number, max));
+    }
+  }
+
+  /**
+   * To be invoked after showing the result dialog.
+   */
+  private void onPostResultDialog() {
+    if (success && playTrack && savedPath != null) {
+      Intent intent = new Intent()
+          .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK)
+          .putExtra(GOOGLE_EARTH_TOUR_FEATURE_ID, KmlTrackWriter.TOUR_FEATURE_ID)
+          .setClassName(GOOGLE_EARTH_PACKAGE, GOOGLE_EARTH_CLASS)
+          .setDataAndType(Uri.fromFile(new File(savedPath)), GOOGLE_EARTH_KML_MIME_TYPE);
+      startActivity(intent);
+    }
+    finish();
+  }
+}
