7f4d47d7b542939b7c2466cbb9bf73d693d44b6a
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2019-09-13 22:38:39 +0200

    Foreground service notification now uses the logo directly (vector).

--- src/main/java/de/dennisguse/opentracks/services/TrackRecordingService.java
+++ src/main/java/de/dennisguse/opentracks/services/TrackRecordingService.java
@@ -425,7 +425,7 @@ public class TrackRecordingService extends Service {
                 .setContentText(getString(messageId))
                 .setContentTitle(getString(R.string.app_name))
                 .setOngoing(true)
-                .setSmallIcon(R.drawable.ic_stat_notify_recording)
+                .setSmallIcon(R.drawable.ic_logo)
                 .setCategory(Notification.CATEGORY_SERVICE)
                 .setWhen(System.currentTimeMillis());
         startForeground(1, builder.build());
