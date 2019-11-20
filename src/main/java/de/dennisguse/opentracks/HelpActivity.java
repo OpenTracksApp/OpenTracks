f2c04eec1afd48d4424121318b67ab6aafd796bf
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2019-11-20 07:27:56 +0100

    Add help activity. Can be accessed from Settings.

---
+++ src/main/java/de/dennisguse/opentracks/HelpActivity.java
@@ -0,0 +1,9 @@
+package de.dennisguse.opentracks;
+
+public class HelpActivity extends AbstractActivity {
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.help;
+    }
+}
