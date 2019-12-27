b585bfe44c4910cff40e93a519fe71c9b4be3dea
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2019-12-27 23:54:29 +0100

    Moved to Github organization: OpenTracksApp.

--- src/androidTest/java/de/dennisguse/opentracks/content/DescriptionGeneratorTest.java
+++ src/androidTest/java/de/dennisguse/opentracks/content/DescriptionGeneratorTest.java
@@ -69,7 +69,7 @@ public class DescriptionGeneratorTest {
         track.setTripStatistics(stats);
         track.setCategory("hiking");
         String expected = //"Created by"
-                "<a href='https://github.com/dennisguse/opentracks'>OpenTracks</a><p>"
+                "<a href='https://github.com/OpenTracksApp/opentracks'>OpenTracks</a><p>"
                 + "Name: -<br>"
                 + "Activity type: hiking<br>"
                 + "Description: -<br>"
