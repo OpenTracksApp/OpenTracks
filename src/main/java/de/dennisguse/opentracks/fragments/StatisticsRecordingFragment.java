56a664aa69287ce7cc2dac1114ca822ffaeeb158
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2020-10-31 10:17:34 +0100

    Move handling of sensor removal (from settings) in BluetoothConnectionManager.BluetoothConnectionManager.

--- src/main/java/de/dennisguse/opentracks/fragments/StatisticsRecordingFragment.java
+++ src/main/java/de/dennisguse/opentracks/fragments/StatisticsRecordingFragment.java
@@ -434,10 +434,7 @@ public class StatisticsRecordingFragment extends Fragment implements TrackDataLi
     }
 
     private void setHeartRateSensorData(SensorDataSet sensorDataSet) {
-        int isVisible = View.VISIBLE;
-        if (PreferencesUtils.isBluetoothHeartRateSensorAddressNone(getContext())) {
-            isVisible = View.GONE;
-        }
+        int isVisible = sensorDataSet != null ? View.VISIBLE : View.GONE;
         heartRateGroup.setVisibility(isVisible);
         setVisibilitySensorHorizontalLine();
 
@@ -457,10 +454,7 @@ public class StatisticsRecordingFragment extends Fragment implements TrackDataLi
     }
 
     private void setCadenceSensorData(SensorDataSet sensorDataSet) {
-        int isVisible = View.VISIBLE;
-        if (PreferencesUtils.isBluetoothCyclingCadenceSensorAddressNone(getContext())) {
-            isVisible = View.GONE;
-        }
+        int isVisible = sensorDataSet != null ? View.VISIBLE : View.GONE;
         cadenceGroup.setVisibility(isVisible);
         setVisibilitySensorHorizontalLine();
 
@@ -538,13 +532,8 @@ public class StatisticsRecordingFragment extends Fragment implements TrackDataLi
 
         // Set activity type
         {
-            activityLabel.setVisibility(trackIconValue != null ? View.VISIBLE : View.GONE);
-
-            activitySpinner.setVisibility(trackIconValue != null ? View.VISIBLE : View.GONE);
             activitySpinner.setEnabled(isRecording);
-            if (trackIconValue != null) {
-                TrackIconUtils.setIconSpinner(activitySpinner, trackIconValue);
-            }
+            TrackIconUtils.setIconSpinner(activitySpinner, trackIconValue);
         }
 
         // Set time
