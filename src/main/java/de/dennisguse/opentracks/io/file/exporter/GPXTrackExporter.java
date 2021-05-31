f1ee0ea0354142f33bc456863774b3081e1a9c9d
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2021-05-31 17:07:41 +0200

    GPX exporter: cleanup.

--- src/main/java/de/dennisguse/opentracks/io/file/exporter/GPXTrackExporter.java
+++ src/main/java/de/dennisguse/opentracks/io/file/exporter/GPXTrackExporter.java
@@ -198,7 +198,6 @@ public class GPXTrackExporter implements TrackExporter {
             printWriter.println("xmlns=\"http://www.topografix.com/GPX/1/1\"");
             printWriter.println("xmlns:topografix=\"http://www.topografix.com/GPX/Private/TopoGrafix/0/1\"");
             printWriter.println("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"");
-            printWriter.println("xmlns:atom=\"http://www.w3.org/2005/Atom\"");
             printWriter.println("xmlns:opentracks=\"http://opentracksapp.com/xmlschemas/v1\"");
             printWriter.println("xmlns:gpxtpx=\"http://www.garmin.com/xmlschemas/TrackPointExtension/v2\"");
             printWriter.println("xmlns:pwr=\"http://www.garmin.com/xmlschemas/PowerExtension/v1\"");
