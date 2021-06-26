a91e1d5d1d3d0b45850f8612f5b3111dd7ffe22a
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2021-06-26 23:23:36 +0200

    Show on map: offer KML instead of KMZ.

--- src/main/java/de/dennisguse/opentracks/ShowOnMapProxyActivity.java
+++ src/main/java/de/dennisguse/opentracks/ShowOnMapProxyActivity.java
@@ -57,10 +57,9 @@ public abstract class ShowOnMapProxyActivity extends AppCompatActivity {
         context.startActivity(Intent.createChooser(intent, context.getString(R.string.open_track_as_trackfileformat, trackFileFormat.getExtension())));
     }
 
-    @Deprecated //TODO I guess we should send KML; zipping should not really improve the situation.
-    public static class KMZ extends ShowOnMapProxyActivity {
-        public KMZ() {
-            super(TrackFileFormat.KMZ_WITH_TRACKDETAIL_AND_SENSORDATA);
+    public static class KML extends ShowOnMapProxyActivity {
+        public KML() {
+            super(TrackFileFormat.KML_WITH_TRACKDETAIL_AND_SENSORDATA);
         }
     }
 
