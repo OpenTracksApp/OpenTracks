977cbb077de96585f5300511f5b7328ac1ce3947
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2021-11-13 13:53:08 +0100

    Cleanup: BLE cycling speed/distance sensor parsing should be the other way around./ Was seemingly still working...

--- src/androidTest/java/de/dennisguse/opentracks/util/BluetoothUtilsTest.java
+++ src/androidTest/java/de/dennisguse/opentracks/util/BluetoothUtilsTest.java
@@ -43,40 +43,40 @@ public class BluetoothUtilsTest {
     @Test
     public void parseCyclingSpeedCadence_crankOnly() {
         BluetoothGattCharacteristic characteristic = new BluetoothGattCharacteristic(BluetoothUtils.CYCLING_SPEED_CADENCE_SERVICE_UUID, 0, 0);
-        characteristic.setValue(new byte[]{0x01, (byte) 0xC8, 0x00, 0x00, 0x00, 0x06, (byte) 0x99});
+        characteristic.setValue(new byte[]{0x02, (byte) 0xC8, 0x00, 0x00, 0x00, 0x06, (byte) 0x99});
 
         // when
         SensorDataCycling.CadenceAndSpeed sensor = BluetoothUtils.parseCyclingCrankAndWheel("address", "sensorName", characteristic);
 
         // then
-        assertEquals(200, sensor.getCadence().getCrankRevolutionsCount());
         assertNull(sensor.getDistanceSpeed());
+        assertEquals(200, sensor.getCadence().getCrankRevolutionsCount());
     }
 
     @Test
     public void parseCyclingSpeedCadence_wheelOnly() {
         BluetoothGattCharacteristic characteristic = new BluetoothGattCharacteristic(BluetoothUtils.CYCLING_SPEED_CADENCE_SERVICE_UUID, 0, 0);
-        characteristic.setValue(new byte[]{0x02, (byte) 0xE1, 0x00, 0x45, (byte) 0x99});
+        characteristic.setValue(new byte[]{0x01, (byte) 0xFF, (byte) 0xFF, 0, 1, 0x45, (byte) 0x99});
 
         // when
         SensorDataCycling.CadenceAndSpeed sensor = BluetoothUtils.parseCyclingCrankAndWheel("address", "sensorName", characteristic);
 
         // then
+        assertEquals(65535 + 16777216, sensor.getDistanceSpeed().getWheelRevolutionsCount());
         assertNull(sensor.getCadence());
-        assertEquals(225, sensor.getDistanceSpeed().getWheelRevolutionsCount());
     }
 
     @Test
     public void parseCyclingSpeedCadence_crankWheel() {
         BluetoothGattCharacteristic characteristic = new BluetoothGattCharacteristic(BluetoothUtils.CYCLING_SPEED_CADENCE_SERVICE_UUID, 0, 0);
-        characteristic.setValue(new byte[]{0x03, (byte) 0xC8, 0x00, 0x00, 0x00, 0x06, (byte) 0x99, (byte) 0xE1, 0x00, 0x45, (byte) 0x99});
+        characteristic.setValue(new byte[]{0x03, (byte) 0xC8, 0x00, 0x00, 0x01, 0x06, (byte) 0x99, (byte) 0xE1, 0x00, 0x45, (byte) 0x99});
 
         // when
         SensorDataCycling.CadenceAndSpeed sensor = BluetoothUtils.parseCyclingCrankAndWheel("address", "sensorName", characteristic);
 
         // then
-        assertEquals(200, sensor.getCadence().getCrankRevolutionsCount());
-        assertEquals(225, sensor.getDistanceSpeed().getWheelRevolutionsCount());
+        assertEquals(200 + 16777216, sensor.getDistanceSpeed().getWheelRevolutionsCount());
+        assertEquals(225, sensor.getCadence().getCrankRevolutionsCount());
     }
 
     @Test
