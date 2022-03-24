f350ee4cf308f3b31eb3f808a9584da6d2b408b8
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2022-03-24 02:17:47 +0100

    UI: only update once every second.
    Accidentally introduced in 78519883c07eeecff930e747433c868e3f631f02.

--- src/main/java/de/dennisguse/opentracks/services/TrackRecordingService.java
+++ src/main/java/de/dennisguse/opentracks/services/TrackRecordingService.java
@@ -46,7 +46,7 @@ public class TrackRecordingService extends Service implements TrackPointCreator.
 
     private static final String TAG = TrackRecordingService.class.getSimpleName();
 
-    private static final Duration RECORDING_DATA_UPDATE_INTERVAL = Duration.ofMillis(100);
+    private static final Duration RECORDING_DATA_UPDATE_INTERVAL = Duration.ofSeconds(1);
 
     public static final RecordingStatus STATUS_DEFAULT = RecordingStatus.notRecording();
     public static final RecordingData NOT_RECORDING = new RecordingData(null, null, null);
