OpenTracksApp/OpenTracks

commit c4b8082efc87a4eb48bc39cb5c7cd91ba1ab10f6

Bluetooth sensors: event-driven TrackPoint creation.
Part of #500.

src/main/java/de/dennisguse/opentracks/services/handlers/GPSHandler.java
@@ -121,7 +121,7 @@ public class GPSHandler implements LocationListener, GpsStatus.GpsStatusListener
         }
 
         lastTrackPoint = trackPoint;
-        trackPointCreator.onNewTrackPoint(trackPoint, thresholdHorizontalAccuracy);
+        trackPointCreator.onNewTrackPoint(trackPoint);
     }
 
     @Override

src/main/java/de/dennisguse/opentracks/services/handlers/TrackPointCreator.java
@@ -20,7 +20,7 @@ import de.dennisguse.opentracks.services.sensors.BluetoothRemoteSensorManager;
 /**
  * Creates TrackPoints while recording by fusing data from different sensors (e.g., GNSS, barometer, BLE sensors).
  */
-public class TrackPointCreator {
+public class TrackPointCreator implements BluetoothRemoteSensorManager.SensorDataSetChangeObserver {
 
     private static final String TAG = TrackPointCreator.class.getSimpleName();
 
@@ -51,10 +51,10 @@ public class TrackPointCreator {
 
         gpsHandler.onStart(context);
 
-        remoteSensorManager = new BluetoothRemoteSensorManager(context);
-        remoteSensorManager.start();
-
+        remoteSensorManager = new BluetoothRemoteSensorManager(context, this);
         altitudeSumManager = new AltitudeSumManager();
+
+        remoteSensorManager.start();
         altitudeSumManager.start(context);
     }
 
@@ -109,17 +109,23 @@ public class TrackPointCreator {
         gpsHandler.onSharedPreferenceChanged(key);
     }
 
-    public synchronized void onNewTrackPoint(@NonNull TrackPoint trackPoint, @NonNull Distance thresholdHorizontalAccuracy) {
+    /**
+     * Got a new TrackPoint from Bluetooth only; contains no GPS location.
+     *
+     * @param sensorDataSet
+     */
+    @Override
+    public void onChange(SensorDataSet sensorDataSet) {
+        onNewTrackPoint(new TrackPoint(TrackPoint.Type.SENSORPOINT, createNow()));
+    }
+
+    public synchronized void onNewTrackPoint(@NonNull TrackPoint trackPoint) {
         fill(trackPoint);
 
-        boolean stored = service.newTrackPoint(trackPoint, thresholdHorizontalAccuracy);
+        boolean stored = service.newTrackPoint(trackPoint, gpsHandler.getThresholdHorizontalAccuracy());
         if (stored) {
             resetSensorData();
         }
-    }
-
-    public void onNewTrackPointWithoutGPS() {
-        onNewTrackPoint(new TrackPoint(TrackPoint.Type.SENSORPOINT, createNow()), gpsHandler.getThresholdHorizontalAccuracy());
     }
 
     public TrackPoint createSegmentStartManual() {

src/androidTest/java/de/dennisguse/opentracks/io/file/importer/ExportImportTest.java
@@ -147,7 +147,7 @@ public class ExportImportTest {
         service.insertMarker("Marker 2", "Marker 2 category", "Marker 2 desc", null);
 
         trackPointCreator.setClock(Clock.fixed(Instant.parse("2020-02-02T02:02:06Z"), ZoneId.of("CET")));
-        trackPointCreator.setRemoteSensorManager(new BluetoothRemoteSensorManager(context));
+        trackPointCreator.setRemoteSensorManager(new BluetoothRemoteSensorManager(context, trackPointCreator));
         service.pauseCurrentTrack();
 
         trackPointCreator.setClock(Clock.fixed(Instant.parse("2020-02-02T02:02:20Z"), ZoneId.of("CET")));
@@ -165,7 +165,7 @@ public class ExportImportTest {
         sendLocation(trackPointCreator, Instant.parse("2020-02-02T02:02:23Z"), 3, 16.001, 10, 15, 10, 0);
 
         trackPointCreator.setClock(Clock.fixed(Instant.parse("2020-02-02T02:02:24Z"), ZoneId.of("CET")));
-        trackPointCreator.setRemoteSensorManager(new BluetoothRemoteSensorManager(context));
+        trackPointCreator.setRemoteSensorManager(new BluetoothRemoteSensorManager(context, trackPointCreator));
         service.endCurrentTrack();
 
         Track track = contentProviderUtils.getTrack(trackId);
@@ -412,7 +412,7 @@ public class ExportImportTest {
     }
 
     private void mockBLESensorData(TrackPointCreator trackPointCreator, Float speed, Distance distance, Float heartRate, Float cyclingCadence, Float power) {
-        trackPointCreator.setRemoteSensorManager(new BluetoothRemoteSensorManager(context) {
+        trackPointCreator.setRemoteSensorManager(new BluetoothRemoteSensorManager(context, trackPointCreator) {
             @Override
             public SensorDataSet fill(@NonNull TrackPoint trackPoint) {
                 SensorDataSet sensorDataSet = new SensorDataSet();

src/androidTest/java/de/dennisguse/opentracks/services/handlers/GPSHandlerTest.java
@@ -62,7 +62,7 @@ public class GPSHandlerTest {
         locationHandler.onLocationChanged(createLocation(45f, 35f, 3, 5, System.currentTimeMillis()));
 
         // then
-        verify(trackPointCreator, times(1)).onNewTrackPoint(any(TrackPoint.class), any(Distance.class));
+        verify(trackPointCreator, times(1)).onNewTrackPoint(any(TrackPoint.class));
     }
 
     /**
@@ -78,7 +78,7 @@ public class GPSHandlerTest {
         locationHandler.onLocationChanged(createLocation(latitude, 35f, 3, 5, System.currentTimeMillis()));
 
         // then
-        verify(trackPointCreator, times(0)).onNewTrackPoint(any(TrackPoint.class), any(Distance.class));
+        verify(trackPointCreator, times(0)).onNewTrackPoint(any(TrackPoint.class));
     }
 
     /**
@@ -94,7 +94,7 @@ public class GPSHandlerTest {
 
         // then
         // no newTrackPoint called
-        verify(trackPointCreator, times(0)).onNewTrackPoint(any(TrackPoint.class), any(Distance.class));
+        verify(trackPointCreator, times(0)).onNewTrackPoint(any(TrackPoint.class));
     }
 
     @Test
@@ -107,7 +107,7 @@ public class GPSHandlerTest {
         locationHandler.onLocationChanged(createLocation(99.0, 35.0, Long.MAX_VALUE, 15, System.currentTimeMillis()));
 
         // then
-        verify(trackPointCreator, times(1)).onNewTrackPoint(any(TrackPoint.class), any(Distance.class));
+        verify(trackPointCreator, times(1)).onNewTrackPoint(any(TrackPoint.class));
     }
 
     /**

src/androidTest/java/de/dennisguse/opentracks/services/TrackRecordingServiceTest.java
@@ -280,7 +280,7 @@ public class TrackRecordingServiceTest {
         trackPointCreator.stopGPS();
         trackPointCreator.setAltitudeSumManager(altitudeSumManager);
 
-        trackPointCreator.onNewTrackPoint(new TrackPoint(TrackPoint.Type.TRACKPOINT, Instant.parse("2020-02-02T02:02:05Z")), Distance.of(50));
+        trackPointCreator.onNewTrackPoint(new TrackPoint(TrackPoint.Type.TRACKPOINT, Instant.parse("2020-02-02T02:02:05Z")));
 
         // then
         assertTrue(service.isRecording());
@@ -404,7 +404,7 @@ public class TrackRecordingServiceTest {
                 new TrackPoint(TrackPoint.Type.TRACKPOINT, Instant.parse("2020-02-02T02:02:03Z"))
                         .setLatitude(10)
                         .setLongitude(10)
-                , Distance.of(50));
+        );
 
         // when
         Marker.Id markerId = service.insertMarker(null, null, null, null);
@@ -452,7 +452,7 @@ public class TrackRecordingServiceTest {
                 .setSpeed(Speed.of(speed))
                 .setBearing(3.0f);
 
-        trackRecordingService.getTrackPointCreator().onNewTrackPoint(trackPoint, Distance.of(50));
+        trackRecordingService.getTrackPointCreator().onNewTrackPoint(trackPoint);
     }
 
     //TODO Workaround as service is not stopped on API23; thus sharedpreferences are not reset between tests.

src/androidTest/java/de/dennisguse/opentracks/services/TrackRecordingServiceTestLocation.java
@@ -325,7 +325,7 @@ public class TrackRecordingServiceTestLocation {
         Track.Id trackId = service.startNewTrack();
         service.stopUpdateRecordingData();
         service.getTrackPointCreator().setAltitudeSumManager(altitudeSumManager);
-        service.getTrackPointCreator().setRemoteSensorManager(new BluetoothRemoteSensorManager(context) {
+        service.getTrackPointCreator().setRemoteSensorManager(new BluetoothRemoteSensorManager(context, service.getTrackPointCreator()) {
 
             @Override
             public boolean isEnabled() {
@@ -386,7 +386,7 @@ public class TrackRecordingServiceTestLocation {
     @MediumTest
     @Test
     public void testOnLocationChangedAsync_idle_withSensorDistance() {
-        BluetoothRemoteSensorManager remoteSensorManager = new BluetoothRemoteSensorManager(context) {
+        BluetoothRemoteSensorManager remoteSensorManager = new BluetoothRemoteSensorManager(context, service.getTrackPointCreator()) {
 
             @Override
             public boolean isEnabled() {

src/androidTest/java/de/dennisguse/opentracks/services/handlers/TrackPointCreatorTest.java
@@ -48,7 +48,7 @@ public class TrackPointCreatorTest {
         Distance horizontalAccuracyThreshold = Distance.of(50);
 
         // when
-        subject.onNewTrackPoint(trackPoint, horizontalAccuracyThreshold);
+        subject.onNewTrackPoint(trackPoint);
 
         // then
         Thread.sleep(10); // Wait for executor service

src/main/java/de/dennisguse/opentracks/services/TrackRecordingService.java
@@ -88,7 +88,6 @@ public class TrackRecordingService extends Service implements TrackPointCreator.
         @Override
         public void run() {
             updateRecordingDataWhileRecording();
-            trackPointCreator.onNewTrackPointWithoutGPS(); //TODO Should not be called every second, right? + with do some duplicate computation with updateRecordingDataWhileRecording().
 
             Handler localHandler = TrackRecordingService.this.handler;
             if (localHandler == null) {

commit d4252b47e3a665374522a96352d584c1ce5145b9

Bugfix: prevent race condition in TrackPointCreator.
This race condition leads to a crash in TrackStatisticsUpdater as the time might appear to move backwards if the created TrackPoints are not stored in order. Might happen if multiple sensors (BLE or GPS) are used in simultaneously.

Every method affected by a race condition is now synchronized.

Problem was introduced in c4b8082efc87a4eb48bc39cb5c7cd91ba1ab10f6

Fixes #1151.

src/main/java/de/dennisguse/opentracks/services/handlers/GPSHandler.java
@@ -12,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
 
 import java.time.Duration;
+import java.time.Instant;
 
 import de.dennisguse.opentracks.R;
 import de.dennisguse.opentracks.data.models.Distance;
@@ -109,25 +110,23 @@ public class GPSHandler implements LocationListener, GpsStatus.GpsStatusListener
             return;
         }
 
-        TrackPoint trackPoint = new TrackPoint(location, trackPointCreator.createNow());
-        boolean isAccurate = trackPoint.fulfillsAccuracy(thresholdHorizontalAccuracy);
-        boolean isValid = LocationUtils.isValidLocation(location);
-
         if (gpsStatus != null) {
+            // Send each update to the status; please note that this TrackPoint is not stored.
+            TrackPoint trackPoint = new TrackPoint(location, Instant.ofEpochMilli(location.getTime()));
             gpsStatus.onLocationChanged(trackPoint);
         }
 
-        if (!isValid) {
+        if (!LocationUtils.isValidLocation(location)) {
             Log.w(TAG, "Ignore newTrackPoint. location is invalid.");
             return;
         }
 
-        if (!isAccurate) {
+        if (!LocationUtils.fulfillsAccuracy(location, thresholdHorizontalAccuracy)) {
             Log.d(TAG, "Ignore newTrackPoint. Poor accuracy.");
             return;
         }
 
-        trackPointCreator.onNewTrackPoint(trackPoint);
+        trackPointCreator.onChange(location);
     }
 
     @Override

src/main/java/de/dennisguse/opentracks/services/handlers/TrackPointCreator.java
@@ -1,6 +1,7 @@
 package de.dennisguse.opentracks.services.handlers;
 
 import android.content.Context;
+import android.location.Location;
 import android.util.Log;
 import android.util.Pair;
 
@@ -47,7 +48,7 @@ public class TrackPointCreator implements BluetoothRemoteSensorManager.SensorDat
         this.gpsHandler = gpsHandler;
     }
 
-    public void start(@NonNull Context context) {
+    public synchronized void start(@NonNull Context context) {
         this.context = context;
 
         gpsHandler.onStart(context);
@@ -71,7 +72,7 @@ public class TrackPointCreator implements BluetoothRemoteSensorManager.SensorDat
         gpsHandler.onStop();
     }
 
-    public void reset() {
+    public synchronized void reset() {
         if (remoteSensorManager == null || altitudeSumManager == null) {
             Log.d(TAG, "No recording running and no reset necessary.");
             return;
@@ -80,7 +81,7 @@ public class TrackPointCreator implements BluetoothRemoteSensorManager.SensorDat
         altitudeSumManager.reset();
     }
 
-    private SensorDataSet fill(TrackPoint trackPoint) {
+    private SensorDataSet addSensorData(TrackPoint trackPoint) {
         if (!isStarted()) {
             Log.w(TAG, "Not started, should not be called.");
             return null;
@@ -98,7 +99,7 @@ public class TrackPointCreator implements BluetoothRemoteSensorManager.SensorDat
         return sensorDataSet;
     }
 
-    public void stop() {
+    public synchronized void stop() {
         gpsHandler.onStop();
 
         if (remoteSensorManager != null) {
@@ -114,16 +115,21 @@ public class TrackPointCreator implements BluetoothRemoteSensorManager.SensorDat
         this.context = null;
     }
 
+    public synchronized void onChange(@NonNull Location location) {
+        onNewTrackPoint(new TrackPoint(location, createNow()));
+    }
+
     /**
      * Got a new TrackPoint from Bluetooth only; contains no GPS location.
      */
     @Override
-    public void onChange(SensorDataSet sensorDataSet) {
+    public synchronized void onChange(@NonNull SensorDataSet unused) {
         onNewTrackPoint(new TrackPoint(TrackPoint.Type.SENSORPOINT, createNow()));
     }
 
-    public synchronized void onNewTrackPoint(@NonNull TrackPoint trackPoint) {
-        fill(trackPoint);
+    @VisibleForTesting
+    public void onNewTrackPoint(@NonNull TrackPoint trackPoint) {
+        addSensorData(trackPoint);
 
         boolean stored = service.newTrackPoint(trackPoint, gpsHandler.getThresholdHorizontalAccuracy());
         if (stored) {
@@ -131,13 +137,13 @@ public class TrackPointCreator implements BluetoothRemoteSensorManager.SensorDat
         }
     }
 
-    public TrackPoint createSegmentStartManual() {
+    public synchronized TrackPoint createSegmentStartManual() {
         return TrackPoint.createSegmentStartManualWithTime(createNow());
     }
 
-    public TrackPoint createSegmentEnd() {
+    public synchronized TrackPoint createSegmentEnd() {
         TrackPoint segmentEnd = TrackPoint.createSegmentEndWithTime(createNow());
-        fill(segmentEnd);
+        addSensorData(segmentEnd);
         reset();
         return segmentEnd;
     }
@@ -158,11 +164,12 @@ public class TrackPointCreator implements BluetoothRemoteSensorManager.SensorDat
             currentTrackPoint.setLatitude(lastStoredTrackPointWithLocation.getLatitude());
         }
 
-        SensorDataSet sensorDataSet = fill(currentTrackPoint);
+        SensorDataSet sensorDataSet = addSensorData(currentTrackPoint);
 
         return new Pair<>(currentTrackPoint, sensorDataSet);
     }
 
+    @VisibleForTesting
     Instant createNow() {
         return Instant.now(clock);
     }

src/main/java/de/dennisguse/opentracks/util/LocationUtils.java
@@ -17,6 +17,8 @@ package de.dennisguse.opentracks.util;
 
 import android.location.Location;
 
+import de.dennisguse.opentracks.data.models.Distance;
+
 /**
  * Utility class for decimating tracks at a given level of precision.
  *
@@ -39,5 +41,12 @@ public class LocationUtils {
         return location != null
                 && Math.abs(location.getLatitude()) <= 90
                 && Math.abs(location.getLongitude()) <= 180;
+    }
+
+    public static boolean fulfillsAccuracy(Location location, Distance thresholdHorizontalAccuracy) {
+        return location.hasAccuracy() &&
+                Distance.of(location.getAccuracy())
+                        .lessThan(thresholdHorizontalAccuracy);
+
     }
 }

src/androidTest/java/de/dennisguse/opentracks/io/file/importer/ExportImportTest.java
@@ -469,7 +469,7 @@ public class ExportImportTest {
                 return sensorDataSet;
             }
         });
-        trackPointCreator.onChange(null);
+        trackPointCreator.onChange(new SensorDataSet());
     }
 
     private void mockAltitudeChange(TrackPointCreator trackPointCreator, float altitudeGain) {

src/androidTest/java/de/dennisguse/opentracks/services/handlers/GPSHandlerTest.java
@@ -22,7 +22,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.time.Instant;
 
 import de.dennisguse.opentracks.data.models.Distance;
-import de.dennisguse.opentracks.data.models.TrackPoint;
 import de.dennisguse.opentracks.settings.PreferencesUtils;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -62,7 +61,7 @@ public class GPSHandlerTest {
         locationHandler.onLocationChanged(createLocation(45f, 35f, 3, 5, System.currentTimeMillis()));
 
         // then
-        verify(trackPointCreator, times(1)).onNewTrackPoint(any(TrackPoint.class));
+        verify(trackPointCreator, times(1)).onChange(any(Location.class));
     }
 
     /**
@@ -78,7 +77,7 @@ public class GPSHandlerTest {
         locationHandler.onLocationChanged(createLocation(latitude, 35f, 3, 5, System.currentTimeMillis()));
 
         // then
-        verify(trackPointCreator, times(0)).onNewTrackPoint(any(TrackPoint.class));
+        verify(trackPointCreator, times(0)).onChange(any(Location.class));
     }
 
     /**
@@ -94,7 +93,7 @@ public class GPSHandlerTest {
 
         // then
         // no newTrackPoint called
-        verify(trackPointCreator, times(0)).onNewTrackPoint(any(TrackPoint.class));
+        verify(trackPointCreator, times(0)).onChange(any(Location.class));
     }
 
     @Test
@@ -107,7 +106,7 @@ public class GPSHandlerTest {
         locationHandler.onLocationChanged(createLocation(99.0, 35.0, Long.MAX_VALUE, 15, System.currentTimeMillis()));
 
         // then
-        verify(trackPointCreator, times(1)).onNewTrackPoint(any(TrackPoint.class));
+        verify(trackPointCreator, times(1)).onChange(any(Location.class));
     }
 
     /**