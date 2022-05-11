2fb18f4140c34b1f227f3f90d76250331bf92008
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2022-05-11 21:27:06 +0200

    Public API: respect post-workout export.
    Fixes #1225.

--- src/main/java/de/dennisguse/opentracks/publicapi/StopRecording.java
+++ src/main/java/de/dennisguse/opentracks/publicapi/StopRecording.java
@@ -1,11 +1,24 @@
 package de.dennisguse.opentracks.publicapi;
 
+import de.dennisguse.opentracks.data.models.Track;
+import de.dennisguse.opentracks.services.RecordingData;
 import de.dennisguse.opentracks.services.TrackRecordingService;
+import de.dennisguse.opentracks.util.ExportUtils;
 
 public class StopRecording extends AbstractAPIActivity {
 
     protected void execute(TrackRecordingService service) {
+        RecordingData recordingData = service.getRecordingDataObservable().getValue();
+        Track.Id trackId = null;
+        if (recordingData != null && recordingData.getTrack() != null) {
+            trackId = recordingData.getTrack().getId();
+        }
+
         service.endCurrentTrack();
+
+        if (trackId != null) {
+            ExportUtils.postWorkoutExport(this, trackId);
+        }
     }
 
     @Override
