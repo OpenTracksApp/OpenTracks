4119d84c5faa36c737d831c6b41ab8fa25f8e025
parth-5097 <parth5097@gmail.com>
2023-03-19 18:50:17 -0400

    #8 #283 resolved conflict and added changes

--- src/main/java/de/dennisguse/opentracks/settings/PreferencesUtils.java
+++ src/main/java/de/dennisguse/opentracks/settings/PreferencesUtils.java
@@ -292,6 +292,11 @@ public class PreferencesUtils {
         return getBoolean(R.string.stats_show_on_lockscreen_while_recording_key, STATS_SHOW_ON_LOCKSCREEN_DEFAULT);
     }
 
+    public static boolean shouldShowAvgSpeedOnTrackRecordedScreen() {
+        final boolean STATS_SHOW_ON_LOCKSCREEN_DEFAULT = resources.getBoolean(R.bool.stats_show_fastest_avg_default);
+        return getBoolean(R.string.stats_show_fastest_avg_key, STATS_SHOW_ON_LOCKSCREEN_DEFAULT);
+    }
+
     public static boolean shouldKeepScreenOn() {
         final boolean DEFAULT = resources.getBoolean(R.bool.stats_keep_screen_on_while_recording_default);
         return getBoolean(R.string.stats_keep_screen_on_while_recording_key, DEFAULT);
