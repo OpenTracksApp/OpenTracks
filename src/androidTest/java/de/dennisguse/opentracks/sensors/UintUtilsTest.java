db20cfca799c641f66d29cb14fe7cbfe3f365c61
rjo12 <119473038+rjo12@users.noreply.github.com>
2023-05-11 22:49:23 +0930

    Update UintUtilsTest.java
    (1)   Corrected one previously existing test case.  Unsigned 16 bit arithmetic is modulo UINT16_MAX + 1, not modulo UINT16_MAX.
    
    (2)   Added tests of modulo arithmetic to allow arguments out of range (the tested function previously threw exceptions in these cases)
    
    (3)   Replicated all the tests to verify correct handling for 32-bit unsigned.

--- src/androidTest/java/de/dennisguse/opentracks/sensors/UintUtilsTest.java
+++ src/androidTest/java/de/dennisguse/opentracks/sensors/UintUtilsTest.java
@@ -8,12 +8,39 @@ public class UintUtilsTest {
 
     @Test
     public void diff() {
-        assertEquals(0, UintUtils.diff(1, 1, UintUtils.UINT16_MAX));
+        assertEquals(0, UintUtils.diff(1, 1, UintUtils.UINT16_MAX));  
         assertEquals(1, UintUtils.diff(2, 1, UintUtils.UINT16_MAX));
         assertEquals(3, UintUtils.diff(5, 2, UintUtils.UINT16_MAX));
-        assertEquals(65534, UintUtils.diff(1, 2, UintUtils.UINT16_MAX));
-
+        assertEquals(65535, UintUtils.diff(1, 2, UintUtils.UINT16_MAX));     /* unsigned 16 arithmetic is modulo UINT16_MAX + 1, not modulo UINT16_MAX */
         assertEquals(65530, UintUtils.diff(UintUtils.UINT16_MAX, 5, UintUtils.UINT16_MAX));
+        
+        /*   Test modulo arithmetic for arguments that are out of range */
+        
+        assertEquals(0, UintUtils.diff(65537, 1, UintUtils.UINT16_MAX));
+        assertEquals(0, UintUtils.diff(1, 65537, UintUtils.UINT16_MAX)); 
+        assertEquals(0, UintUtils.diff(65537, 65537, UintUtils.UINT16_MAX)); 
+        assertEquals(0, UintUtils.diff(-65535, 1, UintUtils.UINT16_MAX));
+        assertEquals(0, UintUtils.diff(1, -65535, UintUtils.UINT16_MAX)); 
+        assertEquals(0, UintUtils.diff(-65535, -65535, UintUtils.UINT16_MAX)); 
+        assertEquals(0, UintUtils.diff(-65535, 65537, UintUtils.UINT16_MAX));
+        assertEquals(0, UintUtils.diff(65537, -65535, UintUtils.UINT16_MAX)); 
+        
+        /*  The following tests are the above, but for 32-bit unsigned. */
+        
+        assertEquals(0, UintUtils.diff(1, 1, UintUtils.UINT32_MAX));  
+        assertEquals(1, UintUtils.diff(2, 1, UintUtils.UINT32_MAX));
+        assertEquals(3, UintUtils.diff(5, 2, UintUtils.UINT32_MAX));
+        assertEquals(4294967295, UintUtils.diff(1, 2, UintUtils.UINT32_MAX));
+        assertEquals(4294967290, UintUtils.diff(UintUtils.UINT32_MAX, 5, UintUtils.UINT32_MAX));
+       
+        assertEquals(0, UintUtils.diff(4294967297, 1, UintUtils.UINT32_MAX));
+        assertEquals(0, UintUtils.diff(1, 4294967297, UintUtils.UINT32_MAX)); 
+        assertEquals(0, UintUtils.diff(4294967297, 4294967297, UintUtils.UINT32_MAX)); 
+        assertEquals(0, UintUtils.diff(-4294967295, 1, UintUtils.UINT32_MAX));
+        assertEquals(0, UintUtils.diff(1, -4294967295, UintUtils.UINT32_MAX)); 
+        assertEquals(0, UintUtils.diff(-4294967295, -4294967295, UintUtils.UINT32_MAX)); 
+        assertEquals(0, UintUtils.diff(-4294967295, 4294967297, UintUtils.UINT32_MAX));
+        assertEquals(0, UintUtils.diff(65537, -4294967297, UintUtils.UINT32_MAX));
     }
 
     @Test
