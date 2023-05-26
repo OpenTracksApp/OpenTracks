2abad51506dc3580b9b447195db1230c17eb3ff3
Chris <Cballenger3@gatech.edu>
2023-05-26 13:56:32 -0400

    Branch work

---
+++ src/main/java/de/dennisguse/opentracks/publicapi/StopRecording.java
@@ -0,0 +1,28 @@
+package de.dennisguse.opentracks.publicapi;
+
+import de.dennisguse.opentracks.data.models.Track;
+import de.dennisguse.opentracks.services.RecordingData;
+import de.dennisguse.opentracks.services.TrackRecordingService;
+import de.dennisguse.opentracks.util.ExportUtils;
+
+public class StopRecording extends AbstractAPIActivity {
+
+    protected void execute(TrackRecordingService service) {
+        RecordingData recordingData = service.getRecordingDataObservable().getValue();
+        Track.Id trackId = null;
+        if (recordingData != null && recordingData.getTrack() != null) {
+            trackId = recordingData.getTrack().getId();
+        }
+
+        service.endCurrentTrack();
+
+        if (trackId != null) {
+            ExportUtils.postWorkoutExport(this, trackId);
+        }
+    }
+
+    @Override
+    protected boolean isPostExecuteStopService() {
+        return true;
+    }
+}
