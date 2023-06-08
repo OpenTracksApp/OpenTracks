65c4b85bc4e42f562611121ddf753f1a80a5e41e
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2023-06-08 07:15:33 +0200

    EspressoTests: only grant POST_NOTIFICATION if running on Android 13.

--- src/androidTest/java/de/dennisguse/opentracks/EspressoEditTrackRecordingTest.java
+++ src/androidTest/java/de/dennisguse/opentracks/EspressoEditTrackRecordingTest.java
@@ -40,11 +40,7 @@ public class EspressoEditTrackRecordingTest {
     public ActivityScenarioRule<TrackListActivity> mActivityTestRule = new ActivityScenarioRule<>(TrackListActivity.class);
 
     @Rule
-    public GrantPermissionRule mGrantPermissionRuleLocation = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
-    @Rule
-    public GrantPermissionRule mGrantPermissionRuleBluetooth = GrantPermissionRule.grant(android.Manifest.permission.BLUETOOTH_CONNECT);
-    @Rule
-    public GrantPermissionRule mGrantPermissionRuleNotifications = GrantPermissionRule.grant(Manifest.permission.POST_NOTIFICATIONS);
+    public GrantPermissionRule mGrantPermissionRule = TestUtil.createGrantPermissionRule();
 
     @LargeTest
     @Test
