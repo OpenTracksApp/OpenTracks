abb51cfa910e0d5de471930fbca0140bb639ad27
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2023-12-30 20:29:09 +0100

    Bugfix: process cycling power sensor data correctly.
    Fixes #1802.

--- src/main/java/de/dennisguse/opentracks/sensors/sensorData/AggregatorCyclingPower.java
+++ src/main/java/de/dennisguse/opentracks/sensors/sensorData/AggregatorCyclingPower.java
@@ -3,16 +3,17 @@ package de.dennisguse.opentracks.sensors.sensorData;
 import androidx.annotation.NonNull;
 
 import de.dennisguse.opentracks.data.models.Power;
+import de.dennisguse.opentracks.sensors.BluetoothHandlerManagerCyclingPower;
 
-public class AggregatorCyclingPower extends Aggregator<Power, Power> {
+public class AggregatorCyclingPower extends Aggregator<BluetoothHandlerManagerCyclingPower.Data, Power> {
 
     public AggregatorCyclingPower(String name, String address) {
         super(name, address);
     }
 
     @Override
-    public void computeValue(Raw<Power> current) {
-        this.value = current.value();
+    public void computeValue(Raw<BluetoothHandlerManagerCyclingPower.Data> current) {
+        this.value = current.value().power();
     }
 
     @NonNull
